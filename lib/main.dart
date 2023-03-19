3e560df0629c06a484440373b64fae1972a8aa22
Takuya <takuya249818@gmail.com>
2023-03-20 07:02:36 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/word_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,150 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo saikyo 3',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ListTest(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ListTest extends StatefulWidget {
+  const ListTest({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ListTest> createState() => _ListTestState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ListTestState extends State<ListTest> {
+  int ListNumber = 0;
+  int total = 0;
+  int displayListNumber = 0;
+  final wordList = WordList();
 
-  void _incrementCounter() {
+  List<List<bool>> _checked =
+      List.generate(28, (_) => List.generate(100, (_) => false));
+
+  void setBandNumber(int inData) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      ListNumber = inData;
+    });
+    // print(ListNumber);
+  }
+
+  void _selectAll() {
+    setState(() {
+      _checked[ListNumber] =
+          List<bool>.filled(_checked[ListNumber].length, true);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('ListViewテスト'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              Text(
+                '英単語を選択する',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 8),
+              Text(
+                '学習したい英単語を追加してください',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 8),
+              // ScrollHorizontalAppBar(),
+              Container(
+                color: Colors.blue,
+                height: 40,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemExtent: 96,
+                  itemCount: 28,
+                  itemBuilder: (context, index) {
+                    displayListNumber = index + 1;
+                    return TextButton(
+                      child: Text(
+                        'リスト$displayListNumber',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        setBandNumber(index);
+                      },
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  itemCount: 100,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3.5,
+                  ),
+                  itemBuilder: (context, index2) {
+                    return Container(
+                      color: Colors.white,
+                      child: Container(
+                        child: CheckboxListTile(
+                          value: _checked[ListNumber][index2],
+                          controlAffinity: ListTileControlAffinity.leading,
+                          onChanged: (value) {
+                            setState(() {
+                              _checked[ListNumber][index2] = value!;
+                            });
+                          },
+                          title: Text(wordList.getEnglish(index2, ListNumber)),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      fixedSize: Size(160, 16),
+                      backgroundColor: Color.fromARGB(255, 198, 255, 255),
+                    ),
+                    child: const Text(
+                      'すべて選択',
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                    onPressed: () {
+                      _selectAll();
+                    },
+                  ),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      fixedSize: Size(160, 16),
+                      backgroundColor: Color.fromARGB(255, 198, 255, 255),
+                    ),
+                    child: const Text(
+                      '選択項目を追加',
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
